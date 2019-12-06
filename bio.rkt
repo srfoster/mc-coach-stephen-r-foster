a4fff411cf3b2c70d8ba529d76a2f330b3d17a76
srfoster <stephen@thoughtstem.com>
2019-12-06 11:58:42 -0800

    Pushing my bio

---
+++ bio.rkt
@@ -0,0 +1,16 @@
+#lang mc-coach/bio
+
+(bio
+ #:name  "Stephen R. Foster"
+ #:profile-pic (bitmap/file "stephen.png")
+ #:coach-story "My family moved to Saudi Arabia when I was three years old.
+                I grew up there, and by the time I was in kindergarten, my family had gotten our first computer.
+                Every day when I would come home from school, my mom would teach me how to program that computer.
+                The thing is, she didn't actually know how to code, so she taught me out of a book that she had gotten from the library.
+                That experience taught me two things from an early age: 1) How to code, and 2) How someone learns to code.
+                I got to watch my mom learn something new right in front of me.  She would read the book, think about it, write code to see if it worked,
+                talk it over with me, etc.  And when she understood things, she would make sure that I understood it too.
+                I'm very grateful to her (my first coding coach), and I've spent the last decade or so trying to pay it forward.
+                I've coached many young students personally.  I've coached adult software engineers.  I've coached other coding coaches.  I've
+                even coached people on how to coach other coding coaches!  (How meta!)  In short: I'm passionate about helping
+                people learn to code, and I'm passionate about helping people learn how to learn.")
